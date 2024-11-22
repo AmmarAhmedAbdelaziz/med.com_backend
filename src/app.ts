5696e2ec1cf6e9a5e844e193e64a5cae7b910141
AmmarAhmedAbdelaziz <ammar.ahmed.abdelaziz@gmail.com>
2024-11-22 14:04:17 +0200

    secondjsskjs

--- src/app.ts
+++ src/app.ts
@@ -3,14 +3,26 @@ import cors from "cors";
 import express, { Application, Request, Response } from "express";
 import routes from "./routes/index";
 const app: Application = express();
+const allowedOrigins = [
+  "https://medical-sage-iota.vercel.app",
+  "https://medical-panel.vercel.app",
+];
 app.use(
   cors({
-    origin: "*",
+    origin: (origin, callback) => {
+      // Allow requests with no origin (e.g., mobile apps or curl requests)
+      if (!origin) return callback(null, true);
+
+      if (allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     credentials: true,
     methods: ["GET", "POST", "PATCH", "DELETE", "PUT"],
   })
 );
-app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(routes);
